540912ee6c52f2c929161b029c236d899a79f198
Moritos7 <rideneaya100@gmail.com>
2021-10-02 17:25:21 +0100

    change project

--- src/App.js
+++ src/App.js
@@ -17,6 +17,41 @@ import PrivateRouter from './Components/PrivateRouter';
 
 function App() {
   const auth = useSelector(state => state.auth)
+
+
+  const Sandwiches_Libanais=[
+    {nom:"Fromage",prix:4000},
+    {nom:"Salami",prix:4000},
+    {nom:"Thon",prix:4200},
+    {nom:"Jambon",prix:4200},
+    {nom:"Chawarma",prix:4500},
+    {nom:"Escalope grillée/pannée",prix:4500},
+    {nom:"kabeb",prix:4600}
+  ]
+
+  const Sandwiches_Makloub=[
+    {nom:"Fromage",prix:4200},
+    {nom:"Salami",prix:4200},
+    {nom:"Thon",prix:4400},
+    {nom:"Jambon",prix:4400},
+    {nom:"Chawarma",prix:4900},
+    {nom:"Escalope grillée/pannée",prix:4900},
+    {nom:"kabeb",prix:5000}
+  ]
+
+  const Supplementaires=[
+    {nom:"Frites",prix:500},
+    {nom:"Mayonnaise",prix:500},
+    {nom:"Ketchup",prix:500},
+    {nom:"BBQ",prix:500},
+  ]
+
+  const Boisson=[
+    {nom:"Eau Minerale 0.5L",prix:800},
+    {nom:"Eau Minerale 1.5L",prix:1500},
+    {nom:"Coca Cola 0.5L",prix:1500},
+    {nom:"Coca Cola 1.5L",prix:3000},
+  ]
   return (
     <Router>
       <Navbars/>
---
+++ src/Components/CardBody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardBody() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardBody
